0669a58214dacad4ed92aa79e63f2df21348d53f
AbdulRaheem Khan <abdulraheemzais@gmail.com>
2024-07-07 10:30:39 +0530

    Added Cursor

--- js/script.js
+++ js/script.js
@@ -1,4 +1,49 @@
-$('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('#overlay').toggleClass('open');
+$("#toggle").click(function () {
+  $(this).toggleClass("active");
+  $("#overlay").toggleClass("open");
+});
+
+let cursor = document.querySelector(".cursor"),
+  cursorScale = document.querySelectorAll(".cursor-scale"),
+  mouseX = 0,
+  mouseY = 0;
+
+gsap.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function () {
+    gsap.set(cursor, {
+      css: {
+        left: mouseX,
+        top: mouseY,
+      },
+    });
+  },
+});
+
+window.addEventListener("mousemove", function (e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+cursorScale.forEach((link) => {
+  link.addEventListener("mouseleave", () => {
+    cursor.classList.remove("grow");
+    cursor.classList.remove("grow-small");
   });
+
+  link.addEventListener("mousemove", () => {
+    cursor.classList.add("grow");
+    if (link.classList.contains("small")) {
+      cursor.classList.remove("grow");
+      cursor.classList.add("grow-small");
+    }
+  });
+});
+
+let arkLogo = document.querySelector(".ark-logo");
+arkLogo.addEventListener("mousemove", () => {
+  arkLogo.textContent = "<AbdulRaheem khan/>";
+});
+arkLogo.addEventListener("mouseleave", () => {
+  arkLogo.textContent = "<ARK/>";
+});
